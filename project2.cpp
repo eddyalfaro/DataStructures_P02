6e7d6e86f395480330a424fc5252a8b5445281a8
eddyalfaro <ehalfaroc@ou.edu>
2021-02-16 18:25:10 -0600

    First commit from local

---
+++ project2.cpp
@@ -0,0 +1,26 @@
+/*
+ * project2.cpp
+ *
+ *  Created on: Feb 16, 2021
+ *      Author: eddy_
+ */
+
+#include "project2.h"
+
+#include <iostream>
+using namespace std;
+
+project2::project2() {
+	// TODO Auto-generated constructor stub
+
+}
+
+project2::~project2() {
+	// TODO Auto-generated destructor stub
+}
+
+int main(){
+
+
+	return 0;
+}
---
+++ project2.h
@@ -0,0 +1,17 @@
+/*
+ * project2.h
+ *
+ *  Created on: Feb 16, 2021
+ *      Author: eddy_
+ */
+
+#ifndef PROJECT2_H_
+#define PROJECT2_H_
+
+class project2 {
+public:
+	project2();
+	virtual ~project2();
+};
+
+#endif /* PROJECT2_H_ */
